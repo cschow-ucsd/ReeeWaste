1bb9b7412ad5bfa018a580d4511321d63124c0ce
Chi Chow <cschow@ucsd.edu>
2020-02-23 21:11:34 -0800

    Kotlin/Native with iOS working

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -15,7 +16,13 @@ kotlin {
         nodejs {
         }
     }
-    ios()
+    ios {
+        binaries {
+            framework {
+                baseName = "common"
+            }
+        }
+    }
 
     sourceSets {
         val ktor_version: String by rootProject.extra
@@ -68,3 +75,32 @@ kotlin {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+val packForXcode by tasks.creating(Sync::class) {
+    val targetDir = File(buildDir, "xcode-frameworks")
+
+    /// selecting the right configuration for the iOS
+    /// framework depending on the environment
+    /// variables set by Xcode build
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val framework = kotlin.targets
+            .getByName<KotlinNativeTarget>("iosX64") // iosX64 for emulator, iosArm64 for device
+            .binaries.also { println("Binaries: $it") }.getFramework(mode)
+    inputs.property("mode", mode)
+    dependsOn(framework.linkTask)
+
+    from({ framework.outputDirectory })
+    into(targetDir)
+
+    /// generate a helpful ./gradlew wrapper with embedded Java path
+    doLast {
+        val gradlew = File(targetDir, "gradlew")
+        gradlew.writeText("#!/bin/bash\n"
+                + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
+                + "cd '${rootProject.rootDir}'\n"
+                + "./gradlew \$@\n")
+        gradlew.setExecutable(true)
+    }
+}
+
+tasks.getByName("build").dependsOn(packForXcode)
