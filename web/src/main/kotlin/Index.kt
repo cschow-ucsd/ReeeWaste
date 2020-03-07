f9cf3b5b2304f913a294d0168a8474d557e663d0
Chi Chow <cschow@ucsd.edu>
2020-03-07 03:51:02 -0800

    Remove CSS require

--- web/src/main/kotlin/Index.kt
+++ web/src/main/kotlin/Index.kt
@@ -1,12 +1,8 @@
-import kotlinext.js.require
-import kotlinext.js.requireAll
 import react.dom.render
 import root.root
 import kotlin.browser.document
 
 fun main() {
-    requireAll(require.context("src", true, js("/\\.css$/")))
-
     render(document.getElementById("root")) {
         root()
     }
