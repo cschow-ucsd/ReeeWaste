8802be7e267a0ede6df0c0450394ea7736c5795e
yuyeon <li.alan180@gmail.com>
2020-03-08 03:56:06 -0700

    added image upload android test

--- android/src/androidTest/java/project/ucsd/reee_waste/android/ImageUploadTest.kt
+++ android/src/androidTest/java/project/ucsd/reee_waste/android/ImageUploadTest.kt
@@ -1,11 +1,31 @@
 package project.ucsd.reee_waste.android
 
+import android.graphics.BitmapFactory
+import io.ktor.client.request.forms.InputProvider
+import io.ktor.utils.io.streams.asInput
+import junit.framework.Assert.assertNotNull
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import project.ucsd.reee_waste.backendless.service.RwService
+import java.nio.ByteBuffer
+
 
 class ImageUploadTest {
     @Test
     fun uploadTest(): Unit = runBlocking {
+        val bitmap = BitmapFactory.decodeFile(
+                "/Users/alanwarren/IdeaProjects/ReeeWaste/android/src/main/res/drawable")
+        val width = bitmap.width
+        val height = bitmap.height
 
+        val size = bitmap.rowBytes * bitmap.height
+        val byteBuffer: ByteBuffer = ByteBuffer.allocate(size)
+        bitmap.copyPixelsToBuffer(byteBuffer)
+        val byteArray = byteBuffer.array()
+        val ioStream = byteArray.inputStream()
+        val ioProvider: InputProvider = InputProvider(byteArray.size.toLong()) { ioStream.asInput() }
+        val response = RwService(BuildConfig.BACKENDLESS_APP_ID, BuildConfig.BACKENDLESS_API_KEY)
+                .postPictureAsync("baseballcap.jpg",ioProvider).await()
+        assertNotNull(response)
     }
 }
